84318286e57e6af71c946ba386dc2a9e326869c3
SCITMASTER34TH <granadojapan@yahoo.co.jp>
2022-12-21 16:14:37 +0900

    css

--- awsstripe-main/myapp/js/realtime_clock.js
+++ awsstripe-main/myapp/js/realtime_clock.js
@@ -1,12 +1,24 @@
 function realtimeClock(){
     
     var rtClock = new Date;
+    var dname = rtClock.getDay(),
+        mo = rtClock.getMonth(),
+        dnum = rtClock.getDate(),
+        hours = rtClock.getHours(),
+        minutes = rtClock.getMinutes();
 
-    var hours = rtClock.getHours();
-    var minutes = rtClock.getMinutes();
-
-    // Convert the hours component to 12-hour format
-    // hours = (hours > 12) ? hours - 12 : hours;
+    // 00月 00日 0曜日
+    var month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+    var week = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"];
+    var days = [
+        "1日","2日","3日","4日","5日","6日","7日","8日","9日","10日",
+        "11日","12日","13日","14日","15日","16日","17日","18日","19日","12日",
+        "21日","22日","23日","24日","25日","26日","27日","28日","29日","30日",
+        "31日"];
+    var ids = ["month", "daynum", "dayname"];
+    var values = [month[mo], days[dnum], week[dname]];
+    for(var i = 0; i < ids.length; i++)
+    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 
     // Pad the hours, minutes and seconds with leading zeros
     hours = ("0" + hours).slice(-2);
